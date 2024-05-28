5dbe8c696126007cc073f208ca2fd5005d12c8a4
[Sergi López] <socelsergi@gmail.com>
2024-05-28 13:50:33 +0200

    9 primeras consultas

---
+++ Tema1_MySQL/EntregaSergiLopez.sql
@@ -0,0 +1,8 @@
+SELECT nombre FROM producto;
+SELECT nombre, precio FROM producto;
+SELECT * FROM producto;
+SELECT nombre, precio, precio * (1.08090) FROM producto;
+SELECT nombre as nombreProducto, precio as precioEuro, precio * (1.08090) as precioDolarEstadounidenc FROM producto;
+SELECT UPPER(nombre) as nombre, precio FROM producto;
+SELECT LOWER(nombre) as nombre, precio FROM producto;
+SELECT nombre, UPPER(LEFT(nombre, 2)) as inicials FROM fabricante;
