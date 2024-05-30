a2c45f7f93a6814c2c9fcea5ff906cfd5aaefe73
[Sergi López] <socelsergi@gmail.com>
2024-05-30 13:46:15 +0200

    autosave

--- Tema1_MySQL/Universidad/EntregaUniversidad.sql
+++ Tema1_MySQL/Universidad/EntregaUniversidad.sql
@@ -3,7 +3,10 @@ SELECT nombre, apellido1, apellido2 FROM universidad.persona WHERE tipo LIKE 'al
 SELECT nombre, apellido1, apellido2, fecha_nacimiento FROM universidad.persona WHERE YEAR(fecha_nacimiento) = 1999 AND tipo LIKE 'alumno';
 SELECT nombre, apellido1, apellido2, nif FROM universidad.persona WHERE tipo LIKE 'profesor' AND telefono IS NULL AND nif LIKE '%K';
 SELECT nombre FROM asignatura WHERE cuatrimestre = 1 AND curso = 3 AND  id_grado = 7;
+
 SELECT persona.nombre, persona.apellido1, persona.apellido2, departamento.nombre AS nombreDepartamento FROM persona 
 JOIN profesor ON persona.id = profesor.id_profesor 
 JOIN departamento ON profesor.id_departamento = departamento.id
 ORDER BY persona.apellido1, persona.apellido2, persona.nombre;
+
+SELECT asignatura.nombre persona;
