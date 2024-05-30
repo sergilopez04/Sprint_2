ff9dc9d0a45416f056cacefff02ebc4e3be46610
[Sergi López] <socelsergi@gmail.com>
2024-05-30 10:49:02 +0200

    3 firsts

--- Tema1_MySQL/Universidad/EntregaUniversidad.sql
+++ Tema1_MySQL/Universidad/EntregaUniversidad.sql
@@ -1,1 +1,3 @@
-SELECT apellido1, apellido2, nombre FROM alumnos;
+SELECT apellido1, apellido2, nombre FROM universidad.persona ORDER BY apellido1 ASC, apellido2 ASC,  nombre ASC;
+SELECT nombre, apellido1, apellido2 FROM universidad.persona WHERE telefono IS NULL;
+SELECT nombre, apellido1, apellido2 FROM universidad.persona WHERE YEAR(fecha_nacimiento) = 1999;
