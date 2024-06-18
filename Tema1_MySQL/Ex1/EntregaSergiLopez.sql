3e66dc3630b96b49a878ae71fa8fd49698217724
[Sergi López] <socelsergi@gmail.com>
2024-06-18 13:04:59 +0200

    Organización queries

--- Tema1_MySQL/Ex1/EntregaSergiLopez.sql
+++ Tema1_MySQL/Ex1/EntregaSergiLopez.sql
@@ -1,3 +1,5 @@
+-- First 20 of Tienda
+
 SELECT nombre FROM producto;
 SELECT nombre, precio FROM producto;
 SHOW COLUMNS FROM producto;
@@ -19,6 +21,8 @@ SELECT nombre, precio FROM producto ORDER BY precio LIMIT 1;
 SELECT nombre, precio FROM producto ORDER BY precio DESC LIMIT 1;
 SELECT nombre FROM producto WHERE codigo = 2;
 
+-- Firsts 9 queries Universidad
+
 SELECT apellido1, apellido2, nombre FROM universidad.persona WHERE tipo LIKE 'alumno' ORDER BY apellido1 ASC, apellido2 ASC,  nombre ASC;
 SELECT nombre, apellido1, apellido2 FROM universidad.persona WHERE tipo LIKE 'alumno' AND telefono IS NULL;
 SELECT nombre, apellido1, apellido2, fecha_nacimiento FROM universidad.persona WHERE YEAR(fecha_nacimiento) = 1999 AND tipo LIKE 'alumno';
@@ -29,12 +33,18 @@ SELECT a.nombre, ce.anyo_inicio, ce.anyo_fin FROM asignatura a JOIN alumno_se_ma
 SELECT DISTINCT d.nombre
 FROM departamento d JOIN profesor p ON d.id = p.id_departamento JOIN asignatura a ON p.id_profesor = a.id_profesor JOIN grado g ON a.id_grado = g.id WHERE g.nombre = 'Grado en Ingeniería Informática (Plan 2015)';
 SELECT DISTINCT p.nombre, p.apellido1, p.apellido2 FROM persona p JOIN alumno_se_matricula_asignatura asa ON p.id = asa.id_alumno JOIN curso_escolar ce ON asa.id_curso_escolar = ce.id WHERE ce.anyo_inicio = 2018 AND ce.anyo_fin = 2019;
+
+-- Left/Right Join
+
 SELECT d.nombre AS nom_departament, p.apellido1, p.apellido2, p.nombre AS nom_professor FROM profesor pr LEFT JOIN departamento d ON pr.id_departamento = d.id JOIN persona p ON pr.id_profesor = p.id ORDER BY d.nombre ASC, p.apellido1 ASC, p.apellido2 ASC, p.nombre ASC;
 SELECT p.nombre, p.apellido1, p.apellido2 FROM profesor pr LEFT JOIN departamento d ON pr.id_departamento = d.id JOIN persona p ON pr.id_profesor = p.id WHERE d.id IS NULL;
 SELECT d.nombre AS nom_departament FROM departamento d LEFT JOIN profesor pr ON d.id = pr.id_departamento WHERE pr.id_departamento IS NULL;
 SELECT a.nombre AS nom_assignatura FROM asignatura a LEFT JOIN profesor pr ON a.id_profesor = pr.id_profesor WHERE a.id_profesor IS NULL;
 SELECT d.nombre FROM departamento d LEFT JOIN profesor pr ON d.id = pr.id_departamento LEFT join asignatura a ON pr.id_profesor = a.id_profesor WHERE a.id IS NULL;
 SELECT COUNT(*) AS totalAlumnes FROM persona p JOIN alumno_se_matricula_asignatura asma ON p.id = asma.id_alumno;
+
+-- Queries Resum
+
 SELECT COUNT(*) AS totalAlumnes99 FROM persona p JOIN alumno_se_matricula_asignatura asma ON p.id = asma.id_alumno WHERE YEAR(p.fecha_nacimiento) = 1999;
 SELECT d.nombre AS nomDepartamento, COUNT(pr.id_profesor) AS nomProfesor FROM departamento d JOIN profesor pr ON d.id = pr.id_departamento GROUP BY d.nombre ORDER BY nomProfesor DESC;
 SELECT d.nombre AS nomDepartamento, COUNT(pr.id_profesor) AS nomProfesores FROM departamento d LEFT JOIN profesor pr ON d.id = pr.id_departamento GROUP BY d.nombre;
