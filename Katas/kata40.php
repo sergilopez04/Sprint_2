41175738ddb91ade9bb12c785eeab27f5a6b217a
[Sergi López] <socelsergi@gmail.com>
2024-06-20 12:36:13 +0200

    Finnished kata

--- Katas/kata40.php
+++ Katas/kata40.php
@@ -1,21 +1,26 @@
-<?
-$expresionCorrecta = "(3 * 4) + (7 + 3)";
-var_dump($expresionCorrecta);
-$expresionIncorrecta = "(3* 5 + (7 *2)";
+<?php
 
-function BuscarErrores(string $expresionEvaluada){
+function BuscarErrores(string $expresionEvaluada) {
     $counterR = 0;
     $counterL = 0;
-    $checker = false;
-    for ($i=0; $i < strlen($expresionEvaluada); $i++) { 
-        if ($expresionEvaluada[$i] === "("){
+    for ($i = 0; $i < strlen($expresionEvaluada); $i++) {
+        if ($expresionEvaluada[$i] === "(") {
             ++$counterL;
-        }
-        elseif ($expresionEvaluada[$i] === ")"){
+        } elseif ($expresionEvaluada[$i] === ")") {
             ++$counterR;
         }
     }
+
+    $retVal = ($counterL === $counterR) ? "L'expressió matemàtica és correcte" . PHP_EOL : "L'expressió és incorrecte" . PHP_EOL;
+    return $retVal;
 }
 
+// Ejemplos de expresiones
+$expresionCorrecta = "(3 * 4) + (7 + 3)";
+$expresionIncorrecta = "(3* 5 + (7 *2)";
+
+// Llamadas a la función
+echo BuscarErrores($expresionCorrecta);
+echo BuscarErrores($expresionIncorrecta);
 
 ?>
